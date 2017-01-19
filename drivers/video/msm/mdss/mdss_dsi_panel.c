7a0a30d94617902285375cfeed51815edaafe51e
droidcore <agusmahardika99@gmail.com>
2017-01-19 13:18:55 +0700

    deleted:    arch/arm64/configs/defconfig 	deleted:    arch/arm64/configs/msm-perf_defconfig 	deleted:    arch/arm64/configs/msm8994-OnePlus2-perf_defconfig 	deleted:    arch/arm64/configs/msm8994-OnePlus2_defconfig 	deleted:    arch/arm64/configs/msm8994-perf_defconfig 	deleted:    arch/arm64/configs/msm8994_defconfig 	deleted:    arch/arm64/configs/msm_defconfig 	deleted:    arch/arm64/configs/oneplus2_defconfig 	deleted:    arch/arm64/configs/yarpiin_defconfig 	modified:   drivers/video/msm/mdss/mdss_dsi_panel.c

--- drivers/video/msm/mdss/mdss_dsi_panel.c
+++ drivers/video/msm/mdss/mdss_dsi_panel.c
@@ -985,20 +985,13 @@ static int mdss_dsi_panel_on(struct mdss_panel_data *pdata)
 		return -EINVAL;
 	}
 
-<<<<<<< HEAD
-	display_on = true;
 
-<<<<<<< HEAD
-=======
 #ifdef CONFIG_POWERSUSPEND
 	set_power_suspend_state_panel_hook(POWER_SUSPEND_INACTIVE);
 #endif
 	
->>>>>>> 78d997f... kernel/power/powersuspend: new PM kernel driver for Android w/o early_suspend v1.5 (faux123/Yank555.lu)
-=======
 	display_on = true;
 
->>>>>>> 04d881d... display: add a simple api to query the display state (on/off) at any point in time
 	pinfo = &pdata->panel_info;
 	ctrl = container_of(pdata, struct mdss_dsi_ctrl_pdata,
 				panel_data);
